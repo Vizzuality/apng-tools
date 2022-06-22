1324d6a651a16a3a47fc063a9e5305d835e1433b
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-06-22 19:58:26 +0200

    Fix build
    TODO: The rest, and automate the dependency build (imgquant and apngopt)

--- binding.cpp
+++ binding.cpp
@@ -1,14 +1,10 @@
 
 #include <stdlib.h>
-#include <emscripten/emscripten.h>
-#include <emscripten/html5.h>
-#include <emscripten/wire.h>
-#include <emscripten/bind.h>
+#include <iostream>
 #include "apngopt.hpp"
 
-using namespace emscripten;
-
-struct Options {
+struct Options
+{
     int deflate_method = 1;
     int iter = 15;
     int min_quality = 0;
@@ -16,33 +12,40 @@ struct Options {
     bool disabled_quant = false;
 };
 
-struct OptimizerResult {
+struct OptimizerResult
+{
     long bufferPtr;
     long size;
 };
 
-int main() { }
+int main(int argc, char **argv)
+{
+    std::cout << "If this shows up the main compiles and runs!" << std::endl;
+}
 
-OptimizerResult optAPNG(long pngBufferPtr, long size, Options options, long callbackPtr) {
+OptimizerResult optAPNG(long pngBufferPtr, long size, Options options, long callbackPtr)
+{
     unsigned int first, loops, coltype;
     std::shared_ptr<APNGOpt> apngOpt = std::make_shared<APNGOpt>(reinterpret_cast<void (*)(float)>(callbackPtr));
     std::vector<APNGFrame> frames;
-    OptimizerResult result = OptimizerResult({ 0, 0 });
+    OptimizerResult result = {0, 0};
 
     int res = apngOpt->load_apng(reinterpret_cast<void *>(pngBufferPtr), size, frames, first, loops);
     if (res >= 0)
     {
         apngOpt->optim_dirty(frames);
         apngOpt->optim_duplicates(frames, first);
-        if(!options.disabled_quant) {
+        if (!options.disabled_quant)
+        {
             apngOpt->optim_image(frames, coltype, options.min_quality, options.max_quality);
         }
-        else {
+        else
+        {
             apngOpt->optim_downconvert(frames, coltype);
         }
 
         long bufferPtr;
-        result.size = (long) apngOpt->save_apng(bufferPtr, frames, first, loops, coltype, options.deflate_method, options.iter);
+        result.size = (long)apngOpt->save_apng(bufferPtr, frames, first, loops, coltype, options.deflate_method, options.iter);
         result.bufferPtr = bufferPtr;
     }
 
@@ -55,23 +58,3 @@ OptimizerResult optAPNG(long pngBufferPtr, long size, Options options, long call
 
     return result;
 }
-
-
-EMSCRIPTEN_BINDINGS(apng_optimizer_module) {
-    value_object<Options>("Options")
-        .field("deflate_method", &Options::deflate_method)
-        .field("iter", &Options::iter)
-        .field("min_quality", &Options::min_quality)
-        .field("max_quality", &Options::max_quality)
-        .field("disabled_quant", &Options::disabled_quant)
-        ;
-    
-    value_object<OptimizerResult>("OptimizerResult")
-        .field("bufferPtr", &OptimizerResult::bufferPtr)
-        .field("size", &OptimizerResult::size)
-        ;
-
-
-    function("optAPNG", &optAPNG);
-}
-
--- build.sh
+++ build.sh
@@ -7,50 +7,17 @@ export LDFLAGS="${OPTIMIZE}"
 export CFLAGS="${OPTIMIZE}"
 export CPPFLAGS="${OPTIMIZE}"
 
-NAME="APNGOptimizerModule"
-
-echo "============================================="
-echo "Start build APNG Optizmizer"
-echo "============================================="
 (
-  emcc \
-  --bind \
+  g++ \
   ${OPTIMIZE} \
   -flto \
-  --closure=1 \
-  -s WASM=1 \
-  -s ASSERTIONS=0 \
-  -s ALLOW_MEMORY_GROWTH=1 \
-  -s SAFE_HEAP=0 \
-  -s ENVIRONMENT=web,worker \
-  -s MODULARIZE=1 \
-  -s EXPORT_ES6=1 \
-  -s EXPORT_NAME=${NAME} \
-  -s USE_ES6_IMPORT_META=0 \
-  -s EXTRA_EXPORTED_RUNTIME_METHODS='["addFunction", "removeFunction"]' \
-  -s RESERVED_FUNCTION_POINTERS=1 \
+  -stdlib=libc++ \
   -I ./apngopt/7z \
   -I ./apngopt/libpng \
   -I ./apngopt/zlib \
   -I ./apngopt/zopfli \
-  -I ./imagequant \
+  -I ./apngopt/imagequant \
   -I ./apngopt \
-  -o ./src/wasm/apng-optimizer.js \
   ./binding.cpp \
-  ./apngopt/7z/*.cc \
-  ./apngopt/libpng/*.c \
-  ./apngopt/zlib/*.c \
-  ./apngopt/zopfli/*.c \
-  ./apngopt/*.cpp \
-  ./imagequant/*.c \
 )
 
-echo "============================================="
-echo "Compiling APNG Optizmizer"
-echo "============================================="
-
-#-s ASYNCIFY \
-#-s ASSERTIONS=1 \
-#-s WASM_MEM_MAX=1024MB \
-#-s USE_PTHREADS=1 \
-#-s PTHREAD_POOL_SIZE=5 \
